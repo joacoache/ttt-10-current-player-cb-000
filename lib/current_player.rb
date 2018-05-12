e29ff7726d60cdbfc8596a6b0b46befddc9808ac
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-12 22:49:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,8 +10,8 @@ end
 
 def current_player(board)
   if turn_count(board) % 2 != 0
-    return "X"
-  elsif turn_count(board) % 2 == 0
     return "O"
+  elsif turn_count(board) % 2 == 0
+    return "X"
   end
 end
