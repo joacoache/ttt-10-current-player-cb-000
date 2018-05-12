8c62718766f3cc07c34035da915026e9a095fca5
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-12 22:39:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,13 +6,6 @@ def turn_count(board)
     end
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |tell|
-    if tell == "X" or tell == "O"
-      counter += 1
-    end
-end
 
 board = ["X", "O", " ", " "]
 turn_count(board)
